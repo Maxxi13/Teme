74e3dbbc927d4e00f31634427a555e086736a8c2
Maxxi13 <102547367+Maxxi13@users.noreply.github.com>
2022-04-03 19:14:18 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+my_list = [7, 8, 9, 2, 3, 1, 4, 10, 5, 6] # declararea listei
+
+my_list1 = sorted(my_list)
+print(my_list1) # afișarea unei alte liste ordonată ascendent
+
+print(sorted(my_list,reverse= True)) # afișarea unei liste ordonată descendent
+
+print(my_list1[1::2]) # afișarea numerelor pare din listă
+
+print(my_list1[::2]) # afișarea numerelor impare din listă
+
+print(my_list1[2::3]) # afișarea elementelor multipli ai lui 3
+
